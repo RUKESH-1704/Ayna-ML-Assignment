6a60f0e6d57a5399d2657eb8b291ac54045a027c
RUKESH K <rukesh2002@gmail.com>
2025-08-05 22:55:58 +0530

    Add files via upload

---
+++ model.py
@@ -0,0 +1,70 @@
+
+import torch
+import torch.nn as nn
+
+class DoubleConv(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(DoubleConv, self).__init__()
+        self.double_conv = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True)
+        )
+
+    def forward(self, x):
+        return self.double_conv(x)
+
+class UNet(nn.Module):
+    def __init__(self, in_channels=1, out_channels=3):
+        super(UNet, self).__init__()
+
+        self.down1 = DoubleConv(in_channels, 64)
+        self.pool1 = nn.MaxPool2d(2)
+
+        self.down2 = DoubleConv(64, 128)
+        self.pool2 = nn.MaxPool2d(2)
+
+        self.down3 = DoubleConv(128, 256)
+        self.pool3 = nn.MaxPool2d(2)
+
+        self.bottleneck = DoubleConv(256, 512)
+
+        self.up3 = nn.ConvTranspose2d(512, 256, kernel_size=2, stride=2)
+        self.up_conv3 = DoubleConv(512, 256)
+
+        self.up2 = nn.ConvTranspose2d(256, 128, kernel_size=2, stride=2)
+        self.up_conv2 = DoubleConv(256, 128)
+
+        self.up1 = nn.ConvTranspose2d(128, 64, kernel_size=2, stride=2)
+        self.up_conv1 = DoubleConv(128, 64)
+
+        self.final_conv = nn.Conv2d(64, out_channels, kernel_size=1)
+
+    def forward(self, x):
+        d1 = self.down1(x)
+        p1 = self.pool1(d1)
+
+        d2 = self.down2(p1)
+        p2 = self.pool2(d2)
+
+        d3 = self.down3(p2)
+        p3 = self.pool3(d3)
+
+        bn = self.bottleneck(p3)
+
+        up3 = self.up3(bn)
+        concat3 = torch.cat([up3, d3], dim=1)
+        u3 = self.up_conv3(concat3)
+
+        up2 = self.up2(u3)
+        concat2 = torch.cat([up2, d2], dim=1)
+        u2 = self.up_conv2(concat2)
+
+        up1 = self.up1(u2)
+        concat1 = torch.cat([up1, d1], dim=1)
+        u1 = self.up_conv1(concat1)
+
+        return self.final_conv(u1)
